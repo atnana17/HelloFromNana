179393a48b02acf17ba0f68ccee7e69bd67dde1c
Alex Rakotoarisolo <salerafiot16@gmail.com>
2021-10-11 19:55:01 +0200

    Update from Alex

--- src/nzama/Main.java
+++ src/nzama/Main.java
@@ -3,6 +3,8 @@ package nzama;
 public class Main {
 
     public static void main(String[] args) {
-	  System.out.println("Hello, my name is Nana");
+
+        System.out.println("Hello, my name is Nana");
+        System.out.println("Hello Nana, my name is Alex");
     }
 }
